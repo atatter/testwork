2a4fa51cf5ef348ade195650ede38a844e6a45fd
aleks <alekstatter@gmail.com>
2016-06-24 14:52:28 +0300

    Starting to code the final idea

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,39 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				files: {
-					'style/style.css' : 'style/main.scss'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
-}
+    grunt.initConfig({
+
+        watch: {
+            sass: {
+                files: ['style/sass/*.scss', 'style/sass/*/*.scss'],
+                tasks: ['sass'],
+            }
+        },
+
+        sass: {                              // Task
+            dist: {                            // Target
+                options: {                       // Target options
+                    style: 'expanded'
+                },
+                files: {                         // Dictionary of files
+                    'style/style.css': 'style/sass/main.scss',       // 'destination': 'source'
+                }
+            }
+        },
+
+        express: {
+          all: {
+            options: {
+              port:8080,
+              hostname:'localhost',
+              bases:['./'],
+              livereload:true
+            }
+          }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express');
+
+    grunt.registerTask('server', ['express', 'watch']);
+};
