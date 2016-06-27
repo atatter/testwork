6d36d91066c0f6713b8daee5fe2850b7df1bcc02
aleks <alekstatter@gmail.com>
2016-06-27 18:43:45 +0300

    Angular routing fixed

--- app/app.js
+++ app/app.js
@@ -3,6 +3,16 @@ angular.module('app', ['ngRoute'])
         '$routeProvider', function($routeProvider) {
             console.log("Jah, ma laen");
             $routeProvider
+                .when('/products',
+                {
+                    controller: 'homeController',
+                    templateUrl: 'app/views/products.html'
+                })
+                .when('/contact',
+                {
+                    controller: 'homeController',
+                    templateUrl: 'app/views/contact.html'
+                })
                 .when('/',
                 {
                     controller: 'homeController',
--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -2,4 +2,7 @@ angular.module("app")
   .controller("homeController",
     function ($scope, $location, $routeParams, homeService) {
       console.log("HomeController started");
+      $scope.finishLoading = function() {
+        console.log("Finished");
+      }
     });
